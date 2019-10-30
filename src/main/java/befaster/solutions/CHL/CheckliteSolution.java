44e555aa47696fc37610a54d471ebb6ea4b84de6
sbx_user1051 <sbx_user1051@169.254.91.133>
2019-10-30 16:28:09 +0000

    Wed Oct 30 16:28:09 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -1,14 +1,22 @@
 package befaster.solutions.CHL;
 
 
+import java.util.Map;
+
 public class CheckliteSolution {
+    Map<String, StockKeepingUnit> inventory;
+
+    public CheckliteSolution(Map<String, StockKeepingUnit> inventory){
+        this.inventory = inventory;
+    }
+
     public Integer checklite(String skus) {
         //Have no idea how this SKU is defined. Run tests to see inputs
         // A shopping basket is not a SKU
 
-
         return 0;
     }
 }
+
 
 
--- src/test/java/befaster/solutions/CHL/CheckLiteTest.java
+++ src/test/java/befaster/solutions/CHL/CheckLiteTest.java
@@ -5,10 +5,16 @@ import org.junit.Before;
 public class CheckLiteTest {
 
     @Before
-    
+    public void setUp() {
+        //Populate Inventory
+        StockKeepingUnit sku1 = new StockKeepingUnit("A", )
+
+
+    }
 
 
 
 
 
 }
+
