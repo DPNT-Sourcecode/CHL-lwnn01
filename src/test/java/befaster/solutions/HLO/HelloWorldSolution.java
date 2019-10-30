a435b2ae9330b746572f02167fbdc7d9202dff07
sbx_user1051 <sbx_user1051@169.254.91.133>
2019-10-30 14:28:12 +0000

    Wed Oct 30 14:28:12 UTC 2019

--- src/test/java/befaster/solutions/HLO/HelloWorldSolution.java
+++ src/test/java/befaster/solutions/HLO/HelloWorldSolution.java
@@ -3,7 +3,7 @@ package befaster.solutions.HLO;
 import org.junit.Before;
 import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.MatcherAssert.equalTo;
+import static org.hamcrest.Matchers.equalTo;
 
 public class HelloWorldSolution {
     private HelloSolution helloSolution;
@@ -15,7 +15,12 @@ public class HelloWorldSolution {
 
     @Test
     public void sayHello() {
-        assertThat(helloSolution.hello("Alun"), equalTo)
+        assertThat(helloSolution.hello("Alun"), equalTo("Hello world"));
+    }
+
+    @Test
+    public void sayHello2() {
+        assertThat(helloSolution.hello(""), equalTo("Hello world"));
     }
 
 }
