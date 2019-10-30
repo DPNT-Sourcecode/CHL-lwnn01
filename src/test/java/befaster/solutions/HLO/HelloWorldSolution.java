cb32cabf169b79dd756decc9eed1b6e4daa296bb
sbx_user1051 <sbx_user1051@169.254.91.133>
2019-10-30 14:24:17 +0000

    Wed Oct 30 14:24:17 UTC 2019

--- src/test/java/befaster/solutions/HLO/HelloWorldSolution.java
+++ src/test/java/befaster/solutions/HLO/HelloWorldSolution.java
@@ -1,4 +1,20 @@
 package befaster.solutions.HLO;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class HelloWorldSolution {
+    private HelloSolution helloSolution;
+    @Before
+    public void setUp() {
+        helloSolution = new HelloSolution();
+
+    }
+
+    @Test
+    public void sayHello() {
+
+    }
+
 }
+
